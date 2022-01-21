fde612177b82236ac0d1bfdcd76900c3ebaba407
orankjawo <orankjawo@gmail.com>
2022-01-22 00:03:47 +0700

    first commit

---
+++ app/buku.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class buku extends Authenticatable
+{
+    //
+
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'judul', 'pengarang',
+    ];
+
+}
